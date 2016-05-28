fea9dffe0a661e1217d172c5c2f14d987ef7075e
Elton Augusto Tamilheiro <elton.augusto.tamilheiro@hotmail.com>
2016-05-28 15:24:16 -0300

    Primeiro commit;

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("test");
+            var col = db.GetCollection<Pessoa>("pessoa");
+
+            var pessoa = col.Find(x => x.name.StartsWith("Elton")).FirstOrDefault();
+        }
+    }
+
+    public class Pessoa
+    {
+        public int _id { get; set; }
+        public string name { get; set; }
+        public string profession { get; set; }
+        public int age { get; set; }
+    }
+
+}
